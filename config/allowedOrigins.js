4435a541a1cb0e1c1e4a5617ab508a39a81c3b9f
Kavindra Chamola <kavindrachamolla889@gmail.com>
2024-07-24 21:33:33 +0530

    Update allowedOrigins.js

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -2,7 +2,7 @@
 const allowedOrigins = [
     'http://localhost:5173',
     'http://localhost:4200',
-    'https://main--bloggnew.netlify.app/'
+    'https://vocal-jelly-faf3cd.netlify.app/'
 
 ];
 
